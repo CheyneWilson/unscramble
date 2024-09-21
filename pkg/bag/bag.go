d66ce5e42f94e46deb78bd471bc31610deb6470c
Cheyne Wilson <dev@cheyne.nz>
2024-09-21 23:31:18 +1000

    feat: Add TakeRandom and FindRandom
    Also:
     * Add Remove
     * Add CountOf
     * Add UniqueItems
    
    Encapsulate internal map within Bag)
    Add tests for new methods

--- pkg/bag/bag.go
+++ pkg/bag/bag.go
@@ -1,12 +1,22 @@
 package bag
 
-type Bag[T comparable] map[T]uint
+import (
+	"fmt"
+	"maps"
+	"math/rand"
+)
+
+type Bag[T comparable] struct {
+	m map[T]int
+}
+type Nothing struct{}
+type Set[T comparable] map[T]Nothing
 
 // New
 // - Create a new Bag filled with the items
 func New[T comparable](items ...T) *Bag[T] {
 	b := new(Bag[T])
-	*b = make(map[T]uint)
+	(*b).m = make(map[T]int)
 	for _, item := range items {
 		b.Add(item)
 	}
@@ -17,31 +27,103 @@ func New[T comparable](items ...T) *Bag[T] {
 // - Add an item to the bag
 func (b *Bag[T]) Add(item T) {
 	if b.Has(item) {
-		(*b)[item] += 1
+		(*b).m[item] += 1
 	} else {
-		(*b)[item] = 1
+		(*b).m[item] = 1
+	}
+}
+
+// UniqueItems
+// - Return a set of all the unique items
+func (b *Bag[T]) UniqueItems() Set[T] {
+	keys := make(Set[T], len((*b).m))
+	for k := range maps.Keys((*b).m) {
+		keys[k] = Nothing{}
+	}
+	return keys
+}
+
+// FindRandom
+// - Return an item at random from the bag. The contents of the bag is unchanged
+func (b *Bag[T]) FindRandom() (T, error) {
+	itemCount := (*b).Count()
+	if itemCount == 0 {
+		var noop T
+		return noop, fmt.Errorf("no items in bag")
+	}
+	items := (*b).toArray()
+
+	item := items[rand.Intn(itemCount)]
+	return item, nil
+}
+
+// TakeRandom
+// - Return an item at random from the bag. The item is removed from the bag
+func (b *Bag[T]) TakeRandom() (T, error) {
+	item, err := (*b).FindRandom()
+	if err == nil {
+		(*b).Remove(item)
+	}
+	return item, err
+}
+
+// toArray
+// - transform the internal map into an array. This is a helper function for FindRandom
+func (b *Bag[T]) toArray() []T {
+	// TODO: If performance is an issue, review the underling map data-structure because FindRandom doesn't map
+	//       efficiently to it. Also, revisit Count()
+	items := make([]T, (*b).Count())
+	i := 0
+	for v, itemCount := range (*b).m {
+		for j := 0; j < itemCount; j++ {
+			items[i] = v
+			i++
+		}
+	}
+	return items
+}
+
+// Remove
+//   - Remove a single item from the bag (if present).
+//     Returns true if an item was removed and false otherwise.
+func (b *Bag[T]) Remove(item T) bool {
+	if b.Has(item) {
+		if count := (*b).m[item]; count <= 1 {
+			delete((*b).m, item)
+		} else {
+			(*b).m[item] -= 1
+		}
+		return true
+	} else {
+		return false
 	}
 }
 
 // Count
 // - Return the total number of items in a bag
-func (b *Bag[T]) Count() uint {
-	var total uint = 0
-	for _, quantity := range *b {
+func (b *Bag[T]) Count() int {
+	var total int = 0
+	for _, quantity := range (*b).m {
 		total += quantity
 	}
 	return total
 }
 
+// CountOf
+// Return the quantity of an item in the bag
+func (b *Bag[T]) CountOf(item T) int {
+	return (*b).m[item]
+}
+
 // UniqueCount
 // - return the total number of unique items in a bag
-func (b *Bag[T]) UniqueCount() uint {
-	return uint(len(*b))
+func (b *Bag[T]) UniqueCount() int {
+	return int(len((*b).m))
 }
 
 // Has
 // - return true if an item is found in a bag
 func (b *Bag[T]) Has(item T) bool {
-	_, present := (*b)[item]
+	_, present := (*b).m[item]
 	return present
 }
--- pkg/bag/preset/tileset_test.go
+++ pkg/bag/preset/tileset_test.go
@@ -1,8 +1,10 @@
 package preset
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
+	"unicode"
 )
 
 func TestCountOfDefaultTileSet(t *testing.T) {
@@ -12,9 +14,44 @@ func TestCountOfDefaultTileSet(t *testing.T) {
 		t.Fatalf(`Bag should have 100 items, got '%d'.`, count)
 	}
 	if unique := bag.UniqueCount(); unique != 27 {
-		fmt.Print(bag)
 		t.Fatalf(`Bag should have 27 unique items, got '%d'.`, unique)
+	}
+}
 
+func TestCountOfItemsInDefaultTileSet(t *testing.T) {
+	bag := NewDefaultTileSet()
+	assert.Equal(t, 9, bag.CountOf("A"))
+	assert.Equal(t, 12, bag.CountOf("E"))
+	assert.Equal(t, 9, bag.CountOf("I"))
+	assert.Equal(t, 8, bag.CountOf("O"))
+	assert.Equal(t, 4, bag.CountOf("U"))
+	assert.Equal(t, 2, bag.CountOf("Y"))
+
+}
+
+func TestBag_ListUniqueItemsOfDefaultTileSet(t *testing.T) {
+	bag := NewDefaultTileSet()
+
+	alphabetPlusWild := bag.UniqueItems()
+	for r := 'a'; r < 'z'; r++ {
+		alpha := string(unicode.ToUpper(r))
+		_, present := alphabetPlusWild[alpha]
+		if !present {
+			t.Fatalf(`Bag is missing '%s'.`, alpha)
+		}
+
+		alpha = "_"
+		_, present = alphabetPlusWild[alpha]
+		if !present {
+			t.Fatalf(`Bag is missing '%s'.`, alpha)
+		}
 	}
 
+	for i := 0; i <= 9; i++ {
+		alpha := strconv.Itoa(i)
+		_, present := alphabetPlusWild[alpha]
+		if present {
+			t.Fatalf(`Bag should not contain any digits '%s'.`, alpha)
+		}
+	}
 }
